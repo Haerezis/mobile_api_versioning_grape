07d7cc745f38c73f0eeb5c3e0c935573c20ac5e0
Thomas Kuntz <thomas.kuntz@hivebrite.com>
2025-01-10 14:52:32 +0100

    Finishing touches

--- app/controllers/api/user/endpoints.rb
+++ app/controllers/api/user/endpoints.rb
@@ -3,7 +3,7 @@ module API
     class Endpoints < API::Base
         prefix "user"
         CURRENT_MAJOR_VERSION = 2
-        CURRENT_MINOR_VERSION = 6
+        CURRENT_MINOR_VERSION = 7
         version "v#{CURRENT_MAJOR_VERSION}.#{CURRENT_MINOR_VERSION}", using: :custom_param
 
         mount API::User::Tests::Root
--- lib/grape/custom_param_versioner.rb
+++ lib/grape/custom_param_versioner.rb
@@ -14,6 +14,19 @@ module Grape
           @semver_versions ||= versions.map { |version| to_semver_version(version) }.compact
         end
 
+
+        # Redefine this method to prevent allowing no version given
+        def before
+          potential_version = Rack::Utils.parse_nested_query(env[Rack::QUERY_STRING])[parameter_key]
+          # Next list is uncommented in upstream, allowing blank version param
+          # return if potential_version.blank?
+
+          version_not_found! unless potential_version_match?(potential_version)
+          env[Grape::Env::API_VERSION] = potential_version
+          env[Rack::RACK_REQUEST_QUERY_HASH].delete(parameter_key) if env.key? Rack::RACK_REQUEST_QUERY_HASH
+        end
+
+        # Redefine this method to handle semver versions
         def potential_version_match?(potential_version)
           semver_potential_version = to_semver_version(potential_version)
           semver_versions.blank? || semver_versions.any? { |v| semver_potential_version[0] == v[0] && semver_potential_version[1] <= v[1] }
